745fb374f580a6bf0d090e88b3379a55b64b16dc
Huy Cuong <17520306@gm.uit.edu.vn>
2019-05-28 09:14:46 +0700

    add sql file

--- student-management/build/classes/database/db.sql
+++ student-management/build/classes/database/db.sql
@@ -1,44 +1,48 @@
---BEGIN
---   FOR cur_rec IN (SELECT object_name, object_type
---                     FROM user_objects
---                    WHERE object_type IN
---                             ('TABLE',
---                              'VIEW',
---                              'PACKAGE',
---                              'PROCEDURE',
---                              'FUNCTION',
---                              'SEQUENCE',
---                              'SYNONYM',
---                              'PACKAGE BODY'
---                             ))
---   LOOP
---      BEGIN
---         IF cur_rec.object_type = 'TABLE'
---         THEN
---            EXECUTE IMMEDIATE    'DROP '
---                              || cur_rec.object_type
---                              || ' "'
---                              || cur_rec.object_name
---                              || '" CASCADE CONSTRAINTS';
---         ELSE
---            EXECUTE IMMEDIATE    'DROP '
---                              || cur_rec.object_type
---                              || ' "'
---                              || cur_rec.object_name
---                              || '"';
---         END IF;
---      EXCEPTION
---         WHEN OTHERS
---         THEN
---            DBMS_OUTPUT.put_line (   'FAILED: DROP '
---                                  || cur_rec.object_type
---                                  || ' "'
---                                  || cur_rec.object_name
---                                  || '"'
---                                 );
---      END;
---   END LOOP;
---END;
+--drop database
+BEGIN
+   FOR cur_rec IN (SELECT object_name, object_type
+                     FROM user_objects
+                    WHERE object_type IN
+                             ('TABLE',
+                              'VIEW',
+                              'PACKAGE',
+                              'PROCEDURE',
+                              'FUNCTION',
+                              'SEQUENCE',
+                              'SYNONYM',
+                              'PACKAGE BODY'
+                             ))
+   LOOP
+      BEGIN
+         IF cur_rec.object_type = 'TABLE'
+         THEN
+            EXECUTE IMMEDIATE    'DROP '
+                              || cur_rec.object_type
+                              || ' "'
+                              || cur_rec.object_name
+                              || '" CASCADE CONSTRAINTS';
+         ELSE
+            EXECUTE IMMEDIATE    'DROP '
+                              || cur_rec.object_type
+                              || ' "'
+                              || cur_rec.object_name
+                              || '"';
+         END IF;
+      EXCEPTION
+         WHEN OTHERS
+         THEN
+            DBMS_OUTPUT.put_line (   'FAILED: DROP '
+                                  || cur_rec.object_type
+                                  || ' "'
+                                  || cur_rec.object_name
+                                  || '"'
+                                 );
+      END;
+   END LOOP;
+END;
+
+commit;
+/
 CREATE TABLE Users (
   userID varchar2(10) PRIMARY KEY,
   userName varchar2(32),
